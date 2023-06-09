31677e8121ca9503e6e9c27e222a3f853dbf3d06
Luís Felipe Ogawa <lfogawa0@gmail.com>
2023-06-08 21:41:50 -0300

    media

---
+++ script.js
@@ -0,0 +1,6 @@
+var n1 = Number(prompt("Digite o primeiro número:"))
+var n2 = Number(prompt("Digite o segundo número:"))
+var n3 = Number(prompt("Digite o terceiro número:"))
+var media = (n1 + n2 + n3) / 3
+
+console.log("A média dos três números é ", media)
